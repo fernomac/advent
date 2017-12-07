7d9866872190d06bf6d3a73e5022c0340e9feabf
David Murray <fernomac@coronet.io>
2017-12-06 21:38:51 -0800

    Day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -20,6 +21,33 @@ func isValid(phrase []string) bool {
 	return true
 }
 
+type sorter []rune
+
+func (s sorter) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s sorter) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s sorter) Len() int {
+	return len(s)
+}
+
+func isMoreValid(phrase []string) bool {
+	lut := make(map[string]struct{})
+
+	for _, word := range phrase {
+		runes := []rune(word)
+		sort.Sort(sorter(runes))
+		if _, ok := lut[string(runes)]; ok {
+			return false
+		}
+		lut[string(runes)] = struct{}{}
+	}
+
+	return true
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -28,12 +56,16 @@ func main() {
 	defer file.Close()
 
 	sum := 0
+	sum2 := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		phrase := strings.Split(scanner.Text(), " ")
 		if isValid(phrase) {
 			sum++
+		}
+		if isMoreValid(phrase) {
+			sum2++
 		}
 	}
 
@@ -42,4 +74,5 @@ func main() {
 	}
 
 	fmt.Println(sum)
+	fmt.Println(sum2)
 }
