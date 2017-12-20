2958f913a6989784ff68439d22f799e6f91ca3d7
David Murray <fernomac@coronet.io>
2017-12-20 15:25:11 -0800

    Part 2

--- day15/main.go
+++ day15/main.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
+func nextA(cur int) int {
+	for {
+		cur = (cur * 16807) % 2147483647
+		if cur%4 == 0 {
+			return cur
+		}
+	}
+}
+
+func nextB(cur int) int {
+	for {
+		cur = (cur * 48271) % 2147483647
+		if cur%8 == 0 {
+			return cur
+		}
+	}
+}
+
 func main() {
+	// a := 65
+	// b := 8921
+
 	a := 883
 	b := 879
 
 	count := 0
 
-	for i := 0; i < 40*1000*1000; i++ {
-		a = (a * 16807) % 2147483647
-		b = (b * 48271) % 2147483647
-
+	for i := 0; i < 5*1000*1000; i++ {
+		a = nextA(a)
+		b = nextB(b)
 		if (a & 0xFFFF) == (b & 0xFFFF) {
 			count++
 		}
